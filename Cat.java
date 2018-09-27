80f2401f78657453cf6707422457bbb8460cc6ac
Administrator <sdfaf>
2018-09-27 08:58:09 +0800

    New New zoo

--- Cat.java
+++ Cat.java
@@ -1,13 +1,16 @@
 public class Cat extends Zoo {
 	private int age;
+	private String act;
 	public Cat(String name, String eat, String cry,int age) {
 		super(name, eat, cry);
 		this.age = age;
+		this.act = act;
 	}
 	
 	
 	public void print() {
 		super.print();
-		System.out.println(" "+age);
+		System.out.println(" "+age+" "+act);
+
 	}
 }
